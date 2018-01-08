c317724dedfd0e9460dc2b3fd7937411858b7dc4
Leon <13667180804@139.com>
2018-01-08 16:53:09 +0800

    bug19920，收音浏览过程中，可以点击暂停中断。

--- src/com/amd/radio/Radio_IF.java
+++ src/com/amd/radio/Radio_IF.java
@@ -203,6 +203,7 @@ public class Radio_IF extends CarService_IF {
 					mServiceIF.radio_setEnable(enable);
 				}
 			} else {
+			    exitRescanAndScan5S(true);
 				mServiceIF.radio_setEnable(enable);
 			}
 		} catch (Exception e) {
